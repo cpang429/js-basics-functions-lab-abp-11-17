9b3d084ff15d3711c044ab75bc42c2f1eabfc85b
Chris Pang <chris_pang429@outlook.com>
2017-11-08 20:16:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+function calculateVertical(start, end){
+start = 1;
+end = 2;
+Let feetInBlock = 264;
+
+//
+let blocksTraveled = (end-start);
+
+return blocksTraveled + feetInBlock;
+}
